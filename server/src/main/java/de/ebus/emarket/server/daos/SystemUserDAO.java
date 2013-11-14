7dd5f0a24f722f051a9c618abd0cdd178230ac64
Philipp Liepert <philipp@phillsoft.de>
2013-11-14 13:29:03 +0100

    Adjusted DAO

--- server/src/main/java/de/ebus/emarket/server/daos/SystemUserDAO.java
+++ server/src/main/java/de/ebus/emarket/server/daos/SystemUserDAO.java
@@ -25,13 +25,7 @@ public class SystemUserDAO extends DAO implements ISystemUserDAO {
 	public SystemUser readSystemUser(String username, String password) {
 		final EntityManager em = getEntityManager();
 		final Query q = em.createQuery("select a from SystemUser a where a.username = '"+username+"' and a.password = '"+password+"'");
-		@SuppressWarnings("unchecked")
-		final List<SystemUser> list = q.getResultList();
-		if (list.isEmpty()){
-			return null;
-		}else{
-			return list.get(0);
-		}
+		return (SystemUser) q.getSingleResult();
 	}
 
 	
