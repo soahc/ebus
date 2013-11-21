e981da40363e677d738f20f2fab8165b560bba42
Philipp Liepert <philipp@phillsoft.de>
2013-11-21 17:07:41 +0100

    build script adjusted

--- buildAndUpdate.py
+++ buildAndUpdate.py
@@ -2,37 +2,32 @@
 import subprocess
 import os 
 import sys
+import shutil
 from time import *
 
-from thread import start_new_thread
+class Artefect:
+	def __init__(self, groupId, artifactId, version):
+		self.groupId = groupId
+		self.artifactId = artifactId
+		self.version = version
+	def getJar(self):
+		return self.artifactId+"-"+self.version+".jar"
+
 
 bundles = [
-"de.ebus.emarket/persistence/0.0.1-SNAPSHOT",
-"de.ebus.emarket/api/0.0.1-SNAPSHOT",
-"de.ebus.emarket/server/0.0.1-SNAPSHOT",
-"de.ebus.emarket/frontend/0.0.1-SNAPSHOT",
+Artefect("de.ebus.emarket","persistence","0.0.1-SNAPSHOT"),
+Artefect("de.ebus.emarket","api","0.0.1-SNAPSHOT"),
+Artefect("de.ebus.emarket","server","0.0.1-SNAPSHOT"),
+Artefect("de.ebus.emarket","frontend","0.0.1-SNAPSHOT"),
 ]
+
+# -----------------------------------------------------
 
 def log(txt,init="---"):
     print strftime(init+" (%H:%M:%S) ", localtime())+txt
 
-def updateBunde(ssh, bundleIdent):
-	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("bundle:install -s mvn:"+bundleIdent)
-	output = ssh_stdout.read()
-	bundleid = output[output.index(':')+2:]
-	ssh.exec_command("bundle:update "+bundleid)
-	log ("update bundle: "+bundleIdent,"---")
-
 if sys.version_info < (2, 7):
 	log ('Python Version must be >= 2.7')
-
-try: 
-	import paramiko
-except ImportError:
-	log ('lib paramiko is required, but not found -> http://www.lag.net/paramiko/','>>>')
-	quit()
-
-#------------------------------------------------------
 
 log ('BuildScript v1.0','***')
 log ('* run mvn build')
@@ -43,23 +38,28 @@ except subprocess.CalledProcessError as e:
 	log ('build failed','>>>')
 	quit()
 
-log ('build success')
+log ('build successfull')
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+# -----------------------------------------------------
 
-log ('* connect servicemix via ssh')
-try:
-	ssh.connect('localhost', port=8101, username='smx', password='smx')
+isPosix = (os.name == 'posix')
+smxPath = '/opt/apache-servicemix/' if (isPosix) else 'c:/apache-servicemix/';
+deployPath = smxPath+'deploy/'
 
-	for bundle in bundles:
-		#start_new_thread(updateBunde, (ssh,bundle))
-		#time.sleep(0.5)
-		updateBunde(ssh,bundle)
-		
+if not os.path.isdir(deployPath):
+	print 'cant find servicemix on '+smxPath
+	quit()
 
-except:
-	log ('connection refused - maybe servicemix not running','>>>') 
-ssh.close()
+for pathentry in os.walk(deployPath, False):
+	for file in pathentry[2]:
+		path = os.path.join(pathentry[0], file)
+		os.unlink(path)
 
+for artefect in bundles:
+	jarName = artefect.getJar()
+	fileSrc = artefect.artifactId+'/target/'+jarName
+	fileDst = deployPath+jarName
+	log ("* deploy "+artefect.getJar())
+	shutil.copy(fileSrc, fileDst)
+	
+log ('deployment successfull')
