c91b0dfd01477e88e2b4ce2d57b6595b7d17a57f
Philipp Liepert <philipp@phillsoft.de>
2014-01-05 17:55:31 +0100

    camel import alpha

---
+++ camelimport/src/main/java/camel/Product.java
@@ -0,0 +1,24 @@
+package camel;
+
+import java.math.BigDecimal;
+
+import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
+import org.apache.camel.dataformat.bindy.annotation.DataField;
+
+@CsvRecord(separator = "\\|", skipFirstLine=true)
+public class Product {
+	
+	@DataField(pos = 1, required=true)
+	private String serialNumber;
+	@DataField(pos = 2, required=true)
+	private String name;
+	@DataField(pos = 3, required=true)
+	private float price;
+	//private String price;
+	
+	public String toString(){
+		return serialNumber + ", "+name+", "+ price;
+	}
+	
+	
+}
